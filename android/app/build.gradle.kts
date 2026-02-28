363c87cfab2f6cfea4d19ea328b52ff88ec78fff
Mythili Muthusamy <mythili.muthusamy@in.nspglobaltech.com>
2026-02-28 19:19:50 +0530

    jks added

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@
     }
 
     android {
-        namespace = "com.example.habitshub"
+        namespace = "com.quivic.habitshub"
         compileSdk = flutter.compileSdkVersion
         ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@
 
         defaultConfig {
             // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-            applicationId = "com.example.habitshub"
+            applicationId = "com.quivic.habitshub"
             // You can update the following values to match your application needs.
             // For more information, see: https://flutter.dev/to/review-gradle-config.
             minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/habitshub/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/habitshub/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.habitshub
+package com.quivic.habitshub
 
 import io.flutter.embedding.android.FlutterActivity
 
