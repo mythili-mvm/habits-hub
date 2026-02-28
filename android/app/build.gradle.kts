8ee33df2d5023a58dd8e7c755e69c90409f8a399
Mythili Muthusamy <mythili.muthusamy@in.nspglobaltech.com>
2026-02-28 19:55:53 +0530

    modified key props

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,49 +1,49 @@
-    plugins {
-        id("com.android.application")
-        id("kotlin-android")
-        // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-        id("dev.flutter.flutter-gradle-plugin")
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+
+    namespace = "com.quivic.habitshub"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
-    android {
-        namespace = "com.quivic.habitshub"
-        compileSdk = flutter.compileSdkVersion
-        ndkVersion = flutter.ndkVersion
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
 
-        compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_17
-            targetCompatibility = JavaVersion.VERSION_17
-            isCoreLibraryDesugaringEnabled = true
-        }
+    defaultConfig {
+        applicationId = "com.quivic.habitshub"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
 
-        kotlinOptions {
-            jvmTarget = JavaVersion.VERSION_17.toString()
-        }
-
-        defaultConfig {
-            // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-            applicationId = "com.quivic.habitshub"
-            // You can update the following values to match your application needs.
-            // For more information, see: https://flutter.dev/to/review-gradle-config.
-            minSdk = flutter.minSdkVersion
-            targetSdk = flutter.targetSdkVersion
-            versionCode = flutter.versionCode
-            versionName = flutter.versionName
-        }
-
-        buildTypes {
-            release {
-                // TODO: Add your own signing config for the release build.
-                // Signing with the debug keys for now, so `flutter run --release` works.
-                signingConfig = signingConfigs.getByName("debug")
-            }
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
         }
     }
 
-    dependencies {
-        coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+        }
     }
-
-    flutter {
-        source = "../.."
-    }
+}
